a97eba25a15618b8e95d3682e2b8426704fa1c91
elaineay <elaineauyang@hotmail.com>
2020-07-20 12:15:57 -0700

    add postbuild script for deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express') // server framework
 const bodyParser = require('body-parser') // get body off network requests
 const cors = require('cors') //middleware Connect/Express
+let path = require('path')
 
 const db = require('./db')
 const dogSpotRouter = require('./routes/dogspot-router')
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.json())
+// app.use(express.static(path.join(__dirname, '../client/build')))
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
